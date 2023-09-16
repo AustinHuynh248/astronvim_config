95ab69eb1c83a74af2bc3143ff9b7ab4b8bf264b
AustinHuynh248 <huynhtransongngoc@gmail.com>
2023-09-16 15:30:44 +0700

    chore: neotree show .config

--- plugins/core.lua
+++ plugins/core.lua
@@ -24,7 +24,7 @@ return {
       filesystem = {
         hijack_netrw_behavior = "open_default",
         filtered_items = {
-          always_show = { ".github", ".gitignore", ".env.*" },
+          always_show = { ".github", ".gitignore", ".env.*", ".config" },
         },
       },
     },

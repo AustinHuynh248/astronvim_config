999f838cedc129f7e924280220b8c56341efec07
ngochuynh <huynhtransongngoc@gmail.com>
2023-09-18 09:24:53 +0700

    chore: add always show neotree

--- plugins/core.lua
+++ plugins/core.lua
@@ -24,7 +24,7 @@ return {
       filesystem = {
         hijack_netrw_behavior = "open_default",
         filtered_items = {
-          always_show = { ".github", ".gitignore", ".env.*", ".config" },
+          always_show = { ".github", ".gitignore", ".env", ".env.development.local_preconfig", ".config" },
         },
       },
     },

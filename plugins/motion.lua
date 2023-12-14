f8428cecaebeebe79959cc82113e1af4e5dc15ca
ngochuynh <huynhtransongngoc@gmail.com>
2023-12-14 09:48:58 +0700

    feat(motion): moving to hop for minimal

--- plugins/motion.lua
+++ plugins/motion.lua
@@ -1,16 +1,32 @@
 return {
   {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    ---@type Flash.Config
+    "smoka7/hop.nvim",
     opts = {},
-  -- stylua: ignore
-  keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-  },
+    keys = {
+      {
+        "s",
+        function() require("hop").hint_words() end,
+        mode = { "n" },
+        desc = "Hop hint words",
+      },
+      {
+        "<S-s>",
+        function() require("hop").hint_lines() end,
+        mode = { "n" },
+        desc = "Hop hint lines",
+      },
+      {
+        "s",
+        function() require("hop").hint_words { extend_visual = true } end,
+        mode = { "v" },
+        desc = "Hop hint words",
+      },
+      {
+        "<S-s>",
+        function() require("hop").hint_lines { extend_visual = true } end,
+        mode = { "v" },
+        desc = "Hop hint lines",
+      },
+    },
   },
 }

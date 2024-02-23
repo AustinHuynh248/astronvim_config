4df2faee10fbb30658823d780d0e6bd7a4d8a53e
ngochuynh <huynhtransongngoc@gmail.com>
2024-02-23 16:38:24 +0700

    feat: add codeium nvim

--- plugins/cmp.lua
+++ plugins/cmp.lua
@@ -2,6 +2,10 @@ return {
   "hrsh7th/nvim-cmp",
   dependencies = {
     { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
+    {
+      "Exafunction/codeium.nvim",
+      config = function() require("codeium").setup {} end,
+    },
   },
   opts = function(_, opts)
     local format_kinds = opts.formatting.format
@@ -9,5 +13,18 @@ return {
       format_kinds(entry, item)
       return require("tailwindcss-colorizer-cmp").formatter(entry, item)
     end
+
+    local cmp = require "cmp"
+    -- modify the sources part of the options table
+    opts.sources = cmp.config.sources {
+      { name = "codeium", priority = 1000 },
+      { name = "nvim_lsp", priority = 750 },
+      { name = "luasnip", priority = 725 },
+      { name = "buffer", priority = 500 },
+      { name = "path", priority = 250 },
+    }
+
+    -- return the new table to be used
+    return opts
   end,
 }
